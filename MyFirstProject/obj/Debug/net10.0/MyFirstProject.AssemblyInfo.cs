6c82c872d43adb73781ee0d65c448bb6336d6f57
Celal <binaycelalselim@gmail.com>
2026-06-19 02:35:05 +0300

    feat: while loop exercise added

--- MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
+++ MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441da52ed09634324678778a3a9282822906a575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5630a9d809f2683eaaf3e4afc3efcd6afbc79acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
