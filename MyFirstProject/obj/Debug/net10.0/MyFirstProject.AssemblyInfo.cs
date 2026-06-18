5630a9d809f2683eaaf3e4afc3efcd6afbc79acd
Celal <binaycelalselim@gmail.com>
2026-06-19 01:40:09 +0300

    feat: break & continue exercises added

--- MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
+++ MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aedfe9366b9afc2aabbb2151d68f0ae02839573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441da52ed09634324678778a3a9282822906a575")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
