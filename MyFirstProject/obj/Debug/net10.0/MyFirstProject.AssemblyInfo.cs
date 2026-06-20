ac1be30a52d119fec6b5853dc84dc55d713f0e4c
Celal <binaycelalselim@gmail.com>
2026-06-21 00:24:14 +0300

    feat: foreach loop exercises added

--- MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
+++ MyFirstProject/obj/Debug/net10.0/MyFirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c82c872d43adb73781ee0d65c448bb6336d6f57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973d4f29e229d25cb32e671dc3013b515d5ab4df")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
