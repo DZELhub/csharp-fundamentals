108675d07afc5394cd513f9894aa93190e3d7860
Celal <binaycelalselim@gmail.com>
2026-07-20 01:51:01 +0300

    Lesson7 → BreakContinue

---
+++ LinkedIn_Learning_CSharp/07-BreakContinue/Program.cs
@@ -0,0 +1,73 @@
+﻿//! Class Notes
+{
+    // int[] values = {15, 7 , 12, 23, 41, 28, 9, 17, 36};
+
+    // foreach (int val in values)
+    // {
+    //     //* The continue statement skips the rest of the loop entirely and jums to the next iteration (if there is one)
+
+    //     if (val >= 20 && val <=29)
+    //     {
+    //         continue;
+    //     }
+
+    //     Console.WriteLine($"The val is currently {val}");
+
+    //     //* The break statement stops the loop and exist
+
+    //     if (val >= 40)
+    //     {
+    //         break;
+    //     }
+    // }
+}
+//! Mini Challenge 1: Skip the negative numbers from array
+{
+    // int[] numbers = {10, 15, -5, 22, -1, 35, 40};
+
+    // foreach (int num in numbers)
+    // {
+    //     if (num < 0)
+    //     {
+    //         continue;
+    //     }
+    //     Console.WriteLine($"Current Number: {num}");
+    // }
+}
+//! Mini Challenge 2: Find the first odd number and stop
+{
+    // int[] values = {2,4,6,8,15,10,12};
+
+    // foreach (int val in values)
+    // {
+    //     Console.WriteLine($"Current Value: {val}");
+    //     if (val % 2 == 1)
+    //     {
+    //         Console.WriteLine("Odd number found.");
+    //         break;
+    //     } 
+    // }
+}
+//! Bonus Challenge: Take number from user, negative number proceed, 100 stop, others sum.
+{
+    // int inputNumber = 0;
+    // int total = 0;
+
+    // while (true)
+    // {
+    //     Console.Write("Enter Number: ");
+    //     inputNumber = Convert.ToInt32(Console.ReadLine());
+
+    //     if (inputNumber < 0)
+    //     {
+    //         continue;
+    //     } else if ( inputNumber == 100)
+    //     {
+    //         break;
+    //     }
+
+    //     total += inputNumber;
+        
+    // }
+    // Console.WriteLine($"Total: {total}");
+}
