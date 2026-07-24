aa58728afeb2191049e799e396a15711c31828a0
Celal <binaycelalselim@gmail.com>
2026-07-24 03:34:28 +0300

    feat: Lesson15 → Function Default Params

---
+++ LinkedIn_Learning_CSharp/15-FunctionDefaultParams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//! Class Notes: Default Params
+{
+    // //* Functions can provide default values for their parameters
+
+    // void PrintWithPrefix(string theStr, string prefix="")
+    // {
+    //     Console.WriteLine($"{prefix} {theStr}");
+    // }
+
+    // //* Test the default parameters
+
+    // PrintWithPrefix("Hello There!");
+    // PrintWithPrefix("Hello There!",">:");
+
+    // //* Call with named params
+
+    // PrintWithPrefix(prefix: "% ", theStr: "Hello There!");
+}
